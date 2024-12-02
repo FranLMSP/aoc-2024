1b0a631faae8e568190b873024f029723b601a54
Franco Colmenarez <info@francoacg.com>
2024-12-02 18:54:14 -0500

    star three

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use std::env;
 
 mod star_one;
 mod star_two;
+mod star_three;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args[1].as_str() {
         "1" => star_one::run(),
         "2" => star_two::run(),
+        "3" => star_three::run(),
         _ => unreachable!(),
     }
 }
